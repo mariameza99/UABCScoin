b3b794372fa435ab46c9e2451b19f0ac76af9a3f
mariameza99 <mariac_099@hotmail.com>
2019-08-28 21:25:06 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# UABCScoin
+ Aplicación UABCScoin
---
+++ src/uabcscoin/UABCScoin.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uabcscoin;
+
+/**
+ *
+ * @author sears
+ */
+public class UABCScoin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
